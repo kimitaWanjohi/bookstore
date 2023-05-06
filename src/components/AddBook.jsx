8f11d4788b2fbe72babc9b248456af56c17b7c99
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-06 17:41:19 +0100

    Add book form

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,6 +1,6 @@
 export default function AddBook() {
   return (
-    <div className="flex items-center justify-between px-2 py-4 shadow-md">
+    <form className="flex items-center justify-between px-2 py-4 shadow-md">
       <input
         className="border border-gray-400 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
         placeholder="Book Title"
@@ -11,6 +11,6 @@ export default function AddBook() {
       >
         Add Book
       </button>
-    </div>
+    </form>
   );
 }
