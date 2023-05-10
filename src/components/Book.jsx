026dc9ee595e2d8749fcefa2e40425dec0f3de1f
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-10 17:35:56 +0100

    Display books

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,7 +1,14 @@
-export default function Book() {
+export default function Book({ book }) {
   return (
     <div className="flex items-center justify-between px-2 py-4 shadow-md">
-      <h1>Book</h1>
+      <div className="flex items-start justify-start flex-col">
+        <h2 className="text-xl font-semibold">
+          {book.title}
+        </h2>
+        <h4 className="text-md italic text-grey-500">
+          {book.author}
+        </h4>
+      </div>
       <button
         type="button"
         className="bg-red-500 hover:bg-red-400 py-1 px-2 text-white rounded-lg shadow-sm"
