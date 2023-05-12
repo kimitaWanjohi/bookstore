eb2c58d67fb2f6c43b4eafd6bc505514f80574a7
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-12 17:49:16 +0100

    Connect redux to api using async thunk

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,41 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { getBooks, postBook, deleteBook } from "../../api";
+
+export const fetchBooks = createAsyncThunk("books/fetchBooks", async () => {
+  const res = await getBooks();
+  return res.data;
+});
+
+export const addBook = createAsyncThunk("books/postBook", async (book) => {
+  await postBook(book);
+});
+
+export const delBook = createAsyncThunk("books/deleteBook", async (id) => {
+  await deleteBook(id);
+});
 
 const initialState = {
-  books: [
-    {
-      id: "item1",
-      title: "The Great Gatsby",
-      author: "John Smith",
-      category: "Fiction"
-    },
-    {
-      id: "item2",
-      title: "Anna Karenina",
-      author: "Leo Tolstoy",
-      category: "Fiction"
-    },
-    {
-      id: "item3",
-      title: "The Selfish Gene",
-      author: "Richard Dawkins",
-      category: "Nonfiction"
-    }
-  ],
+  books: [],
+  loading: "idle",
 };
 
 export const booksSlice = createSlice({
   name: "books",
   initialState,
-  reducers: {
-    addBook: (state, action) => {
-      state.books.push(action.payload);
-    },
-    removeBook: (state, action) => {
-      state.books = state.books.filter(book => book.id !== action.payload);
-    }
-  }
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        const keys = Object.keys(action.payload);
+        keys.forEach((id) => {
+          state.books.push({ id, ...action.payload[id][0] });
+        });
+      })
+      .addCase(addBook.fulfilled, (state, action) => {
+        state.books.push({
+          id: action.meta.arg.item_id,
+          ...action.meta.arg,
+        });
+      })
+      .addCase(delBook.fulfilled, (state, action) => {
+        state.books = state.books.filter((book) => book.id !== action.meta.arg);
+      });
+  },
 });
-
-export const { addBook, removeBook } = booksSlice.actions;
 
 export default booksSlice.reducer;
