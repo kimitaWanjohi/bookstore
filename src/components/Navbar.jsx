b299eb9156fab3882b0aaf630ced4292af0bdca4
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-12 21:08:02 +0100

    Fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,29 @@
 import { useNavigate } from "react-router-dom";
+import { BsFillPersonFill } from "react-icons/bs";
 
 export default function Navbar() {
   const navigate = useNavigate();
   return (
-    <nav className="bg-gray-100 shadow-lg  p-4 justify-between w-full">
+    <nav className="shadow-lg  p-4 justify-between w-full">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-blue-500 text-3xl text-bold">Book</h1>
-        <div className="flex gap-8">
+        <div className="flex gap-8 items-baseline">
+          <h1 className="text-blue-500 text-3xl text-bold">Book</h1>
           <button
-            className="hover:scale-105  bg-gray-200 rounded-md hover:bg-white text-blue-500 px-2 py-1"
+            className="hover:scale-105 rounded-md text-blue-500 px-2"
             type="button"
             onClick={() => navigate("/")}
           >
             Home
           </button>
           <button
-            className="hover:scale-105  bg-gray-200 rounded-md hover:bg-white text-blue-500 px-2 py-1"
+            className="hover:scale-105 rounded-md text-blue-500 px-2"
             type="button"
             onClick={() => navigate("/categories")}
           >
             Categories
           </button>
         </div>
+        <BsFillPersonFill size={30} className="text-blue-500" />
       </div>
     </nav>
   );
