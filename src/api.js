9c273a6600435952cac8b3106674493705fd712d
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-12 17:47:39 +0100

    fix delete api call

--- src/api.js
+++ src/api.js
@@ -1,18 +1,17 @@
 import axios from 'axios';
 import { APP_ID, API_URL } from './consts';
 
-
 export const getBooks = async () => {
-  const data = await axios.get(`${API_URL}/apps/${APP_ID}/books`);
-  return data;
-}
+  const res = await axios.get(`${API_URL}/apps/${APP_ID}/books`);
+  return res;
+};
 
 export const postBook = async (book) => {
-  const data = await axios.post(`${API_URL}/apps/${APP_ID}/books`, data=book);
-  return data;
-}
+  const res = await axios.post(`${API_URL}/apps/${APP_ID}/books`, book);
+  return res;
+};
 
 export const deleteBook = async (id) => {
-  const data = await axios.post(`{API_URL}/apps/${APP_ID}/books/${id}`);
-  return data;
+  const res = await axios.delete(`${API_URL}/apps/${APP_ID}/books/${id}`);
+  return res;
 };
