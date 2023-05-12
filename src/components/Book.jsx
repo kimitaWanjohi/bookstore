e703a7531cf4f755c9c7a7a5a1c357ca9fd1f296
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-12 21:49:22 +0100

    Fix book styling

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,11 +1,13 @@
 import { useDispatch } from "react-redux";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 import { delBook } from "../redux/books/booksSlice";
 
 export default function Book({ book }) {
   const dispatch = useDispatch();
   return (
     <div className="flex items-center justify-between px-2 py-4 shadow-md">
-      <div className="flex items-start justify-start flex-col">
+      <div className="flex w-1/4 items-start justify-start flex-col">
         <span className="text-sm text-gray-500">
           {book.category}
         </span>
@@ -16,13 +18,20 @@ export default function Book({ book }) {
           {book.author}
         </span>
       </div>
-      <button
-        type="button"
-        onClick={() => dispatch(delBook(book.id))}
-        className="bg-blue-500 hover:bg-blue-400 py-1 px-2 text-white rounded-lg shadow-sm"
-      >
-        Delete Book
-      </button>
+      <div className="h-[60px] w-[60px]">
+        <CircularProgressbar value={50} text="50%" />
+      </div>
+      <div className="flex items-start justify-start flex-col gap-1">
+        <h4 className="text-md font-light">Current Chapter</h4>
+        <h3 className="text-md">Chapter 18</h3>
+        <button
+          type="button"
+          onClick={() => dispatch(delBook(book.id))}
+          className="bg-blue-500 hover:bg-blue-400 py-2 px-4 text-white rounded-sm shadow-sm"
+        >
+          Delete Book
+        </button>
+      </div>
     </div>
   );
 }
