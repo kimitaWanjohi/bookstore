39b994075f0eb28f6537dedfa6b5b6c772d06b53
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-05-12 21:15:39 +0100

    Fix add book form

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -21,21 +21,21 @@ export default function AddBook() {
 
   return (
     <form onSubmit={handleAddbook} className="flex flex-col md:flex-row items-center gap-2 justify-between px-2 py-4 shadow-md">
-      <div className="flex flex-col md:flex-row gap-2">
+      <div className="w-full flex flex-col md:flex-row gap-2">
         <input
           onChange={(e) => setTitle(e.target.value)}
-          className="border border-gray-400 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+          className="border w-full md:w-3/4 border-gray-400 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
           placeholder="Book Title"
         />
         <input
           onChange={(e) => setAuthor(e.target.value)}
-          className="border border-gray-400 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
+          className="border w-full md:w-1/4 border-gray-400 rounded-lg px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent"
           placeholder="Book Author"
         />
       </div>
       <button
         type="submit"
-        className="bg-blue-500 hover:bg-blue-400 py-1 px-2 text-white rounded-lg shadow-sm"
+        className="bg-blue-500 w-full md:w-[10rem] hover:bg-blue-400 py-2 px-4 text-white rounded-sm shadow-sm"
       >
         Add Book
       </button>
